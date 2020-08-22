5c442cd6ba87f87a50746b41002a37c6fffaf11a
John Astill <john@astill.org>
2020-08-22 02:00:56 -0400

    First version with debugger attaching in Catalina.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PlatformIOEspProgCatalina
 Platform IO setup for Catalina Mac Pro setup
+
+
+Step that got ESP-PROG to connect:
+sudo kextunload /Library/Extensions/FTDIUSBSerialDriver.kext
---
+++ TestDebug/src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+int counter = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  counter++;
+}
