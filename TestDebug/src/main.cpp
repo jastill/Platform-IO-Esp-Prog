0210a5ee68ebf55d07dad6c61305b18e9ca8afe6
John Astill <john@astill.org>
2020-08-22 23:02:44 -0400

    Added some simple incrementing code

--- TestDebug/src/main.cpp
+++ TestDebug/src/main.cpp
@@ -1,12 +1,15 @@
 #include <Arduino.h>
 
 int counter = 0;
+int runner = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  runner++;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   counter++;
+  runner+=2;
 }
